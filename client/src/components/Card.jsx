c57b60b57806e539f1708d4b43603c068c64c138
Kelvin Collado <kelvin@liniax.com>
2021-07-26 21:33:05 -0400

    updates

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,20 +1,12 @@
-import React from "react"
-import {Link} from "@reach/router"
-const Card = props =>{
-    return(<div className="card" style={{width: "18rem"}}>
-    <div className="card-body">
-      <h5 className="card-title"> {props.object.title}</h5>
-      <h6 className="card-subtitle mb-2 text-muted">{props.object.price}</h6>
-      <p className="card-text">{props.object.description}</p>
-    </div>
-    <nav className="navbar navbar-light" style={{backgroundColor: '#ff9933'}}>
-        <div className="container-fluid navcontainer">
+import React from "react";
 
-        <Link style={{flex: '1'}} to={`/view/${props.object._id}`} >View</Link>
-        <Link style={{flex: '1'}} to={`/update/${props.object._id}`}>Update</Link>
-        <Link style={{flex: '1'}} to={`/delete/${props.object._id}`}  >Delete</Link>
-        </div>
-    </nav>
-  </div>)
-}
-export default Card
+const Card = (props) => {
+  const { spiel } = props;
+
+  return (
+    <div className="text-center mt-3">
+      <h4>{spiel.scriptName}</h4>
+    </div>
+  );
+};
+export default Card;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,24 +1,62 @@
-import React, { useEffect, useState } from "react"
-import Card from "../components/Card"
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import axios from "axios";
+import { Link, navigate } from "@reach/router";
 
-const Home = props => {
-    const [allObjects,setAllObjects]= useState(null);
-    useEffect(()=>{
-        axios.get("http://localhost:8000/api/objects")
-        .then(res => setAllObjects(res.data))
-        .catch(err => console.log(err))
-    },[])
-    return(
+const Home = (props) => {
+  const [spiels, setSpiels] = useState(null);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/spiels")
+      .then((res) => setSpiels(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    <div >
-        <h1>this is the home page</h1>
-        {
-            allObjects?
-            allObjects.map((Object, i)=>{return<Card Object= {Object}/>}):""
-        }
-        
-    </div>
-    );
-}
-export default Home
+  const handleCardLink = (spielID) => {
+    navigate(`/view/${spielID}`);
+  };
+
+  return (
+    <>
+      <div className="container mt-4" style={{ textAlign: "left" }}>
+        <Link to={`/add`}>Add</Link>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          gap: ".2em",
+          height: "80vh",
+          marginTop: "1em",
+        }}
+      >
+        {spiels
+          ? spiels.map((spiel, i) => {
+              return (
+                <>
+                  <div
+                    style={{
+                      border: "1px solid lightgrey",
+                      borderRadius: "35px 35px 35px 35px",
+                      height: "30%",
+                      cursor: "pointer",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                    className="col-4"
+                    onClick={() => handleCardLink(spiel._id)}
+                  >
+                    <Card key={i} spiel={spiel} />
+                  </div>
+                </>
+              );
+            })
+          : ""}
+      </div>
+    </>
+  );
+};
+export default Home;
