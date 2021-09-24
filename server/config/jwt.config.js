16877bc0f60254f4f2b2a2df88ca18a4734cb1bd
Kelvin Collado <kelvin@liniax.com>
2021-09-24 16:57:48 -0400

    merge

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -3,23 +3,22 @@ const jwt = require("jsonwebtoken");
 
 // Secret Key dotenv https://www.npmjs.com/package/dotenv
 // require('dotenv').config();
-const secret = "secrets are included"
+const secret = "secrets are included";
 module.exports.secret = secret;
 
-
-const payload = {
-    id: user._id
-};
+// const payload = {
+//     id: user._id
+// };
 // // // using the SECRET_KEY from our .env file
 // const userToken = jwt.sign(payload, secret);
 
 module.exports.authenticate = (req, res, next) => {
-    jwt.verify(req.signedCookies.usertoken, secret, (err, payload) => {
-        console.log(res => console.log(res))
-        if (err) { 
-            res.status(401).json({verified: false});
-        } else {
-            next();
-        }
-    });
-}
+  jwt.verify(req.signedCookies.usertoken, secret, (err, payload) => {
+    console.log((res) => console.log(res));
+    if (err) {
+      res.status(401).json({ verified: false });
+    } else {
+      next();
+    }
+  });
+};
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,52 +1,55 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const {secret} = require("../config/jwt.config");
-
+const { secret } = require("../config/jwt.config");
 
 class UserController {
-    register(req, res) {
-        const user = new User(req.body);
-        user.save()
-            .then((user) => {
-                res
-                    .cookie("usertoken", jwt.sign({_id:user._id}, secret, {httpOnly: true}))
-                    .json({msg: "Success!", user: user})
-                    console.log("msg from backend successfully registered!")
-            })
-            .catch(err => res.json(err))
-    }
+  register(req, res) {
+    const user = new User(req.body);
+    user
+      .save()
+      .then((user) => {
+        res
+          .cookie(
+            "usertoken",
+            jwt.sign({ _id: user._id }, secret, { httpOnly: true })
+          )
+          .json({ msg: "Success!", user: user });
+        console.log("msg from backend successfully registered!");
+      })
+      .catch((err) => res.json(err));
+  }
 
-    login (req, res) {
-        User.findOne({email:req.body.email})
-            .then(user => {
-                if (user == null) {
-                    res.json({msg:"Email not found"}) // email is not found
-                }
-                else {
-                    bcrypt.compare(req.body.password, user.password, (err, result) => {
-                        if (err || !result) {
-                            console.log("invalid login");
-                            res.json({msg:"Invalid login attempt"}) //incorrect pw
-                        }else{
-                            console.log("true");
-                            res.cookie("usertoken", jwt.sign({_id: user._id}, secret), {httpOnly:true})
-                                .json({msg:"success!"})
-                        }
-                    })
-                }
-            })
-            .catch(err => console.log(err));
-    }
+  login(req, res) {
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (user == null) {
+          res.json({ msg: "Email not found" }); // email is not found
+        } else {
+          bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if (err || !result) {
+              console.log("invalid login");
+              res.json({ msg: "Invalid login attempt" }); //incorrect pw
+            } else {
+              console.log("true");
+              res
+                .cookie("usertoken", jwt.sign({ _id: user._id }, secret), {
+                  httpOnly: true,
+                })
+                .json({ msg: "success!" });
+            }
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 
-    getLoggedInUser(req, res) {
-        const decodedJWT = jwt.decode(req.cookies.usertoken, {complete:true});
-        User.findById(decodedJWT.payload._id)
-            .then(user => res.json(user))
-            .catch(err => res.json(err))
-    }
+  getLoggedInUser(req, res) {
+    const decodedJWT = jwt.decode(req.cookies.usertoken, { complete: true });
+    User.findById(decodedJWT.payload._id)
+      .then((user) => res.json(user))
+      .catch((err) => res.json(err));
+  }
 }
-
-
 
 module.exports = new UserController();
